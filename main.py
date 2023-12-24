1c45830754874a07ebce55f80f7d18a94e4664d3
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2023-12-24 00:24:26 -0300

    testando coisas

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from PyTech import create_app
-from flask import redirect, request, render_template, url_for
+from flask import redirect, request, render_template, url_for, Flask
 import mysql.connector
 from flask_hashing import Hashing
 
 app = create_app()
 
-# db = mysql.connector.connect(
-#     host='localhost',
-#     user='root',
-#     password='labinfo',
-#     database='PyTech'
-# )
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='010705',
+    database='PyTech'
+)
 
 hashing = Hashing(app)
 
@@ -21,6 +21,54 @@ def produto(produto):
 @app.route("/user/usuario")
 def usuario(usuario):
     return render_template('productPage.html')
+
+@app.route("/admin")
+def admin():
+    return render_template('admin.html')
+
+@app.route('/cadastrarProduto', methods=['POST'])
+def enviar():
+    i = request.form['idi']
+    m = request.form['mens']
+    a = request.files['arq']
+    
+    ### Descobrir a extensao ###
+    extensao = a.filename.rsplit('.',1)[1]
+    '''
+    foto.png.jpg > "foto.png.jpg".rsplit('.',1) > ['foto.png', 'jpg'][1] > jpg
+    '''
+
+    caminho = f'PyTech/static/img/produtos/{i}.{extensao}'
+    print(caminho)
+    a.save(caminho)
+    
+    cursor = db.cursor(dictionary=True)
+
+    sql = ("INSERT INTO produto "
+           "(NomeProduto, Preco) "
+           "VALUES (%s, %s)")
+
+    tupla = (i, m)
+
+    select = (f"SELECT idproduto "
+              "FROM produto "
+              "WHERE NomeProduto = '{i}'")
+    cursor.execute(select)
+    fetchdata = cursor.fetchall()
+    print(fetchdata)
+    
+    sql2 = ("INSERT INTO imagemproduto "
+        "(Caminho, produto_idproduto) "
+        "VALUES (%s, %s)")
+
+    tupla2 = (caminho, fetchdata)
+    
+    cursor.execute(sql2, tupla2)
+    cursor.execute(sql, tupla)
+    db.commit()
+    cursor.close()
+
+    return ('<h1>Deu Bom</h1>')
 
 if __name__ == '__main__':
     app.run(debug=True)
